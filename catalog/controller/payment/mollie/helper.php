232b703f5fe0c64992018024b7a1ec4e461abc93
Ricardo Jacobs <ricardo@mollie.com>
2016-03-17 09:57:07 +0100

    Updating dev to version 6.3.0 tag.

--- catalog/controller/payment/mollie/helper.php
+++ catalog/controller/payment/mollie/helper.php
@@ -1,7 +1,7 @@
 <?php
 class MollieHelper
 {
-	const PLUGIN_VERSION = "6.2.9";
+	const PLUGIN_VERSION = "6.3.0";
 
 	// All available modules. These should correspond to the Mollie_API_Object_Method constants.
 	const MODULE_NAME_BANKTRANSFER = "banktransfer";
