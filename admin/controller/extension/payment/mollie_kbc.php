4e893ed3f62a50a8bf1e8715097c8e843aa67271
Quality Works <development@qualityworks.eu>
2018-04-17 19:00:11 +0200

    Delete mollie_kbc.php

--- admin/controller/extension/payment/mollie_kbc.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . "/mollie/base.php");
-
-class ControllerExtensionPaymentMollieKbc extends ControllerExtensionPaymentMollieBase
-{
-	const MODULE_NAME = MollieHelper::MODULE_NAME_KBC;
-}
